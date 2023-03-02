3aab56731ecc893323aa0742633de74435f117a6
DT7Solutions <developer@dt7solutions.com>
2023-03-02 11:18:53 +0530

    fixed contact form

--- subhujo/static/js/custom.js
+++ subhujo/static/js/custom.js
@@ -18,12 +18,12 @@ $(document).on('submit', '#contact-form', function(event){
         },  
            
         success:function(data){
-            alert('message sending sucessfully!')
+            alert('Your message has been received, We will contact you soon.')
             $('contact-form')[0].reset();
             $('.contact-form-alert').append("Your message has been received, We will contact you soon.")
         },
         error:function(data){
-            alert('message sending falil!')
+            alert('Your message has been faild, please try agian.')
             $('.contact-form-alert').append("Your message has been faild, please try agian.")
         }
     })
